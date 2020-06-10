21f5770d3f6b2f08186444071ec762b8ebfa2302
BritishPandaChick <sbartley1080@gmail.com>
2020-06-10 00:57:19 -0400

    Debug JS variables. Start debugging the code inside current JS functions

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('choice-text'));
-const progressText = document.getElementById('progressText');
-const scoreText = document.getElementById('score');
-const progressBarFull = document.getElementById('progressBarFull');
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progresText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -50,8 +50,8 @@ startGame = () => {
 };
 
 getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        //got to the end of the page 
+    if (availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS) {
+        //go to the end page 
         return window.location.assign("/end.html");
     }
     questionCounter++;
@@ -64,29 +64,28 @@ getNewQuestion = () => {
     question.innerText = currentQuestion.question;
 
     choices.forEach(choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number];
     });
 
     availableQuestions.splice(questionIndex, 1);
-    acceptingAnswers = true;
+    acceptinganswers = true;
 };
 
 choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return;
+    choice.addEventListener("click", e => {
+        if (!acceptingAnswers) return;
 
-        acceptingAnswers = false;
+        accpetingAnswers = false;
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
+        const selectedAnswer = selectedChoice.dataset["number"];
 
-        const classToApply = 
-            selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+        const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-        if (classToApply == "correct") {
+        if (classToApply === "correct") {
             incrementScore(CORRECT_BONUS);
         }
-        
+
         selectedChoice.parentElement.classList.add(classToApply);
 
         setTimeout(() => {
