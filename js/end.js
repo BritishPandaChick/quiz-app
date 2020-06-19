76c6f1fb17115bd92b2e6825fe653b0768aa7f33
BritishPandaChick <sbartley1080@gmail.com>
2020-06-19 17:03:39 -0400

    Debug HTML and JS files.

--- js/end.js
+++ js/end.js
@@ -1,7 +1,7 @@
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
-const mostRecentScore = localStorage.getItem('mostRecentScore');
 const finalScore = document.getElementById("finalScore");
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
@@ -26,5 +26,5 @@ savedHighScore = e => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign("/");
+    window.location.assign("index.html");
 };
