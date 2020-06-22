01c4299f3b1c681d9d63888fb83df0a5b2b5c2cf
BritishPandaChick <sbartley1080@gmail.com>
2020-06-22 02:11:53 -0400

    Remove code for game, end, and script files. Add new code for files

--- js/end.js
+++ js/end.js
@@ -1,7 +1,7 @@
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
-const mostRecentScore = localStorage.getItem('mostRecentScore');
+const mostRecentScore = localStorage.getItem("mostRecentScore");
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
@@ -10,21 +10,21 @@ const MAX_HIGH_SCORES = 5;
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
-    saveScoreBtn.disabled = !username.value;
+  saveScoreBtn.disabled = !username.value;
 });
 
-savedHighScore = e => {
-    console.log("clicked the save button");
-    e.preventDefault();
+saveHighScore = e => {
+  console.log("clicked the save button!");
+  e.preventDefault();
 
-    const score = {
-        score: Math.floor(Math.random()*100),
-        name: username.value
-    };
-    highScores.push(score);
-    highScores.sort((a,b) => b.score - a.score);
-    highScores.splice(5);
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
 
-    localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign("index.html");
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("/");
 };
