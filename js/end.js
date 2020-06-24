d9788a390da2685ef0987bc10aea4be15f874ae6
BritishPandaChick <sbartley1080@gmail.com>
2020-06-24 01:16:39 -0400

    Debug highscores.html and end files

--- js/end.js
+++ js/end.js
@@ -1,7 +1,7 @@
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
-const mostRecentScore = localStorage.getItem('mostRecentScore');
+const mostRecentScore = localStorage.getItem("mostRecentScore");
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
@@ -13,18 +13,18 @@ username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value;
 });
 
-savedHighScore = e => {
-    console.log("clicked the save button");
+saveHighScore = e => {
+    console.log("clicked the save button!");
     e.preventDefault();
 
     const score = {
-        score: Math.floor(Math.random()*100),
+        score: Math.floor(Math.random() * 100),
         name: username.value
     };
     highScores.push(score);
     highScores.sort((a,b) => b.score - a.score);
     highScores.splice(5);
 
-    localStorage.setItem('highScores', JSON.stringify(highScores));
+    localStorage.setItem("highScores", JSON.stringify(highScores));
     window.location.assign("index.html");
 };
