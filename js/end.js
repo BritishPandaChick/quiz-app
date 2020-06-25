0d3d840d0befd25f2b522a31c40abb121c3b70b4
BritishPandaChick <sbartley1080@gmail.com>
2020-06-25 00:49:07 -0400

    Debug end.html and highscores.html files. Tried debugging saveHighScore function in end.js

--- js/end.js
+++ js/end.js
@@ -7,7 +7,7 @@ const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 const MAX_HIGH_SCORES = 5;
 
-finalScore.innerText = mostRecentScore;
+finalScore.innerText= = mostRecentScore;
 
 username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value;
@@ -26,5 +26,5 @@ saveHighScore = e => {
     highScores.splice(5);
 
     localStorage.setItem("highScores", JSON.stringify(highScores));
-    window.location.assign("index.html");
+    window.location.assign("highscores.html");
 };
