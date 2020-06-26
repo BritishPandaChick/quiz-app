e9658341ab072d348261ca36f964a479c686ba65
BritishPandaChick <sbartley1080@gmail.com>
2020-06-26 00:31:46 -0400

    Debug end.html and end.js

--- js/end.js
+++ js/end.js
@@ -7,24 +7,24 @@ const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 const MAX_HIGH_SCORES = 5;
 
-finalScore.innerText= = mostRecentScore;
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
-    saveScoreBtn.disabled = !username.value;
+  saveScoreBtn.disabled = !username.value;
 });
 
 saveHighScore = e => {
-    console.log("clicked the save button!");
-    e.preventDefault();
+  console.log("clicked the save button!");
+  e.preventDefault();
 
-    const score = {
-        score: Math.floor(Math.random() * 100),
-        name: username.value
-    };
-    highScores.push(score);
-    highScores.sort((a,b) => b.score - a.score);
-    highScores.splice(5);
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5);
 
-    localStorage.setItem("highScores", JSON.stringify(highScores));
-    window.location.assign("highscores.html");
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("/");
 };
